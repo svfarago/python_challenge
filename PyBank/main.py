eae4fd6347e9ab8eb23663b41883343a41a05d71
gitsusan <svfarago@gmail.com>
2020-12-20 19:13:33 -0700

    working code - opens PyBank file into  main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,31 +5,22 @@ import os
 import csv
 
 # csvpath is string value the path that is joined together as a string
-csvpath = os.path.join('..', 'resources', 'budget_data.csv')
+csvpath = os.path.join("Resources", "budget_data.csv")
 
-#csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
-#windows = '..//python_challenge//PyBank//Resources//budget_data.csv
+#Read using open method open(csvpath) as csvfile
+csvfile = open(csvpath)
 
-
-#Read using CSV module using open method using input/output method
-
-with open(csvpath) as csvfile:
-
-#csvfile = open(csvpath)
-
-# CSV reader = opens csv file using reader method; specifies delimiter and variable that holds contents
-#SVF - csvreader
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-#[[header1, header2],
-#    [data1, data2]]
+#Opens csv file using reader method; delimiter and variable that holds contents
+csvreader = csv.reader(csvfile, delimiter=',')
 
 print(csvreader)
 
-# Read the header row first (skip this step if there is now header)
-#csv_header = next(csvreader)
-#print(f"CSV Header: {csv_header}")
+# Reads header
+csv_header = next(csvreader)
+print(f"CSV Header: {csv_header}")
 
-# Read each row of data after the header
+# Reads data
 for row in csvreader:
     print(row)
+
+#POTATO - Start here all working above.
