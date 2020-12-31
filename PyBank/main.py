6808ed56610aee9aea8e0f90b27b6241369867e9
gitsusan <svfarago@gmail.com>
2020-12-31 14:37:30 -0700

    PyBank working up to calculations and screen output

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,55 +1,52 @@
-# Module in Python os--for path across operating systems
+# What: Module in Python paths across operating systems
 import os
 
-# Module in Python csv--for reading CSV files
+# What: Module in Python reads CSV files
 import csv
 
 # csvpath is a string value--creates joined path as string -- /resources/budget_data.csv
 csvpath = os.path.join("Resources", "budget_data.csv")
 
-# Method open -- Read using open method open(csvpath) as csvfile
+# What: Method open -- Read using open method open(csvpath) as csvfile
 csvfile = open(csvpath)
 
-# Opens csv file using reader method; delimiter and variable holds contents--comma in this case
+# VARIABLES - Create placeholder to iterate through specific rows
+total_months = []
+total_profit = []
+monthly_profit_change = []
+
+
+# What: Opens csv file using reader method
+# Notes: Delimiter and variable holds contents--comma in this case
 csvreader = csv.reader(csvfile, delimiter=',')
 
-print(csvreader)
 
-# Reads header--f converts all to string
+# What: Reads header in csv file
+# Notes: To print it to the screen use >> print(f"CSV Header: {csv_header}") | f converts all to string
 csv_header = next(csvreader)
-print(f"CSV Header: {csv_header}")
 
-# Reads data as a row
-for row in csvreader:
-    print(row)
+# What: Iterate through the rows
+for row in csvreader: 
 
+    # Put the total months and total profit values to their corresponding lists
+    # Notes: append means to add to [ ] above | int converts profit to whole number
+    total_months.append(row[1])
+    total_profit.append(int(row[1]))
 
-# CALCULATIONS
+# What: Iterate through the profits in order to get the monthly change in profits
+# Notes: i is a container = | len = returns number of items in container | -1 is to keep total_profit in range, otherwise results in "out of range" error
+# Potato: "-1" - Got error if not used. See "out of range" in Notes above.
+for i in range(len(total_profit)-1):
+        
+    # Take the difference between two months and append to monthly profit change
+     monthly_profit_change.append(total_profit[i+1]-total_profit[i])
 
-
-
-
-# EXPORT text file with results
-
-#Specify file output
-output_path = os.path.join("Analysis", "budget_results.csv")
-
-#Open file with "write" mode and specify variable to hold the contents--with closes file after
-with open (output_path, 'w') as csvfile:
-
-    #initialize csv.writer
-    csvwriter = csv.writer(csvfile, delimiter=',')
-
-    #write header row
-    csvwriter.writerow (['Date','Profit/Losses'])
-
-    # Write second row
-    csvwriter.writerow (['Jan-2010','867884'])
-
-
-
-
-
+# Printout
+print("   ")
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {len(total_months)}")
+print(f"Total: ${sum(total_profit)}")
 
 
 
