2190622beb9aa72ca2e8059a48f805a59f082c39
gitsusan <svfarago@gmail.com>
2020-12-31 17:38:30 -0700

    PyBank almost complete.

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,7 +8,7 @@ import csv
 csvpath = os.path.join("Resources", "budget_data.csv")
 
 # What: Method open -- Read using open method open(csvpath) as csvfile
-csvfile = open(csvpath)
+#csvfile = open(csvpath)
 
 # VARIABLES - Create placeholder for future content | [ ] signifies an empty list.
 # Notes: Only these three require a container for calculations. Increase and decrease profits data is just a lookup of existing data.
@@ -16,35 +16,38 @@ total_months = []
 total_profit = []
 average_change = []
 
-
 # What: Opens csv file using reader method
-# Notes: Delimiter and variable holds contents--comma in this case
-csvreader = csv.reader(csvfile, delimiter=',')
+with open(csvpath) as budget:
+
+     # Notes: Delimiter and variable holds contents--comma in this case
+    csvreader = csv.reader(budget,delimiter=",") 
 
 
-# What: Reads header in csv file
-# Notes: To print it to the screen use >> print(f"CSV Header: {csv_header}") | f converts all to string
-csv_header = next(csvreader)
+    # What: Reads header in csv file
+    # Notes: To confirm file is being read print it to the screen using >> print(f"CSV Header: {csv_header}") | f converts all to string
+    header = next(csvreader)
 
-# What: Iterate through the rows
-for row in csvreader: 
+    # What: Iterate through the rows
+    for row in csvreader: 
 
-    # Puts the total months and total profit values to their corresponding lists using [0] for first column and [1] for second column
-    # Notes: append means to go through all rows and act as a container | int converts profit to number
-    # POTATO: 
-    total_months.append(row[0])
-    total_profit.append(int(row[1]))
+        # Puts the total months and total profit values to their corresponding lists using [0] for first column and [1] for second column
+        # Notes: append means to go through all rows and act as a container | int converts profit to number
+        # POTATO: Confirm I'm understanding "append" correctly
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
 
-# What: Iterate through the profits in order to get the average change in profits
-# Notes: i is a container = | len = returns number of items in a container or list | -1 is to keep total_profit in range, otherwise results in "out of range" error
-# Potato: "-1" - Got error if not used. See "out of range" in Notes above.
-for i in range(len(total_profit)-1):
+    # What: Iterate through the profits in order to get the average change in profits
+    # Notes: i is a container = | len = returns number of items in a container or list | -1 is to keep total_profit in range, otherwise results in "out of range" error
+    # Potato: "-1" - Got error if not used. See "out of range" in Notes above.
+    for i in range(len(total_profit)-1):
         
-    # Take the difference between two months and append to average change
-    # Notes compares each month to next and adds the profit to the "i" and keeps a rolling count [i+1] using square brackets as a temp container (i plus the next one below)
-    # Then subtrack the current from previous month to get the deltas (change in price). Then find the average.See calc in spreadsheet for more notes
-    #POTATO - I think I'm missing basic math. Why "-total_profit[i]"? I played around with the "-" | its right in Excel but uses average.
-     average_change.append(total_profit[i+1]-total_profit[i])
+        # Take the difference between two months and append to average change
+        # Notes compares each month to next and adds the profit to the "i" and keeps a rolling count [i+1] using square brackets as a temp container (i plus the next one below)
+        # Then subtrack the current from previous month to get the deltas (change in price). Then find the average.See calc in spreadsheet for more notes
+        #POTATO - I think I'm missing basic math. Why "-total_profit[i]"? I played around with the "-" | its right in Excel but uses average.
+        average_change.append(total_profit[i+1]-total_profit[i])
+
+
 
 #POTATO - My increase and decrease profits are not right ($25 and $43)
 # What: Finds greatest increase and decrease
@@ -57,7 +60,7 @@ max_increase = average_change.index(max(average_change)) + 1
 max_decrease = min(average_change)
 max_decrease = average_change.index(min(average_change)) + 1 
 
-# What: Print
+# What: Prints
 # Notes: len counts # items in a container | Use "round" and "2" to round to second decimal otherwise really long decimal number
 # Using {} to convert info to string format 
 print("   ")
@@ -68,4 +71,19 @@ print(f"Total: ${sum(total_profit)}")
 print(f"Average Change: {round(sum(average_change)/len(average_change),2)}")
 print(f"Greatest Increase in Profits: {total_months[max_increase]} (${(str(max_increase))})")
 print(f"Greatest Decrease in Profits: {total_months[max_decrease]} (${(str(max_decrease))})")
+
+
+# What: Outputs to text file
+# Notes: "w" for write | \n = hard return
+# POTATO - output doesn't go to "analysis" folder. I created a string path at the start but couldn't get it to work.
+# POTATO - Are the {} just placeholders with /n as hard returns for above lines? There are 7 lines and 7 sets of {} /n
+output = open("output.txt", "w")
+line1 = "Financial Analysis"
+line2 = "----------------------------"
+line3 = str(f"Total Months: {len(total_months)}")
+line4 = str(f"Total: ${sum(total_profit)}")
+line5 = str(f"Average Change: ${round(sum(average_change)/len(average_change),2)}")
+line6 = str(f"Greatest Increase in Profits: {total_months[max_increase]} (${str(max_increase)})")
+line7 = str(f"Greatest Decrease in Profits: {total_months[max_decrease]} (${str(max_decrease)})")
+output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7))
 
--- README.md
+++ README.md
@@ -36,9 +36,12 @@ None
 == Troubleshooting ===============
 Resources used to build this code:
 Help and code review from other students
+
+PyBank:
 https://www.programiz.com/python-programming/methods/list/append
 https://linuxcasual.wordpress.com/2020/03/12/python-round-and-square-brackets/
 https://lerner.co.il/2018/06/08/python-parentheses-primer/
+https://stackoverflow.com/questions/17658055/how-can-i-remove-carriage-return-from-a-text-file-with-python
 
 URLs last used: December 31, 2020
 
