2ea9e16ee990b37eef0a79b36ce9a75c1e2a51cb
gitsusan <svfarago@gmail.com>
2021-01-03 09:30:32 -0700

    Working on percentage v1

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -15,7 +15,8 @@ csvpath = os.path.join("Resources", "election_data.csv")
 all_votes = []
 candidates = []
 percent_votes = []
-number_votes = []
+candidate_votes = []
+percent_votes = []
 #total_votes = 0
 
 # What: Opens csv file using reader method
@@ -39,23 +40,36 @@ with open(csvpath) as election:
         #What: Create unique candidate list using "candidate []" container above to hold, along with a vote in his/her name.
         # Notes: [2]=column 3, so this scans through the entire list and if the candidate name hasn't already been found
             # and put into the candidates container [], then it appends (adds) the candidate name to the list. 
-            # Else = if candidate's name is already in the list, don't re-add but DO count their vote (number_votes)
+            # Else = if candidate's name is already in the list, don't re-add but DO count their vote (candidate_votes)
             # Append adds new candidate to the end of the existing candidates list & adds number of votes to the votes list.
             # POTATO - review this for understanding > Append and index are python list methods. White index must be defined otherwise error
         if row[2] not in candidates:
             candidates.append(row[2])
             index = candidates.index(row[2])
-            number_votes.append(1)
+            candidate_votes.append(1)
         else:
             index = candidates.index(row[2])
-            number_votes[index] += 1
+            candidate_votes[index] += 1
 
     
+    #What:Calculates % of candidate votes
+    # Notes: Iterate through the rows
+    #for votes in candidate_votes: 
+
+    # Puts the percent of votes values to their corresponding lists using [0] for first column
+    # POTATO - Notes: must make candidate_votes and all_votes integer otherwise can't divide lists, must divide numbers
+
+        #percent = (candidate_votes/all_votes) * 100
+        #percent = round(percent)
+        #percent = "%.3f%%" % percent # POTATO: try "%3f" (example 7 at python reference)
+        #percent_votes.append(percent)
+
+
     # What: Flags winning candidate.
-    # Notes: winner looks at max number of votes | POTATO - clarify index references.
-    winner = max(number_votes)
-    index = number_votes.index(winner)
-    winning_candidate = candidates[index]
+    # Notes: winner looks at max number of candidate votes | POTATO - clarify index references.
+    winner = max(candidate_votes)
+    index = candidate_votes.index(winner)
+    winner_candidate = candidates[index]
 
 # What: Prints
 # Notes: len counts # items in a container | Use "round" and "2" to round to second decimal otherwise really long decimal number
@@ -65,10 +79,10 @@ print("----------------------------")
 print(f"Total Votes: {len(all_votes)}")
 print("----------------------------")
 for i in range(len(candidates)):
-    print(f"{candidates[i]}: ({str(number_votes[i])})")
-# Use above "if" and don't use this as it just lists all candidates and then all votes > print(f"name: {candidates} (${(str(number_votes))}) percent votes")
+    print(f"{candidates[i]}: ({str(candidate_votes[i])})")  #POTATO > add this once % is fixed ({str(percent_votes[i])})
+# Use above "if" and don't use this as it just lists all candidates and then all votes > print(f"name: {candidates} (${(str(candidate_votes))}) percent votes")
 print("----------------------------")
-print(f"Winner: {winning_candidate}")
+print(f"Winner: {winner_candidate}")
 print("----------------------------")
 
 
@@ -79,9 +93,9 @@ line1 = "Election Results"
 line2 = "----------------------------"
 line3 = str(f"Total Votes: {len(all_votes)}")
 line4 = "----------------------------"
-line5 = str(f"{candidates[i]}: ({str(number_votes[i])})")
+line5 = str(f"{candidates[i]}: ({str(candidate_votes[i])})")
 line6 = "----------------------------"
-line7 = str(f"Winner: {winning_candidate}")
+line7 = str(f"Winner: {winner_candidate}")
 line8 = "----------------------------"
 output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7,line8))
 
